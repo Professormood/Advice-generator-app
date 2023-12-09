32701a0ff591d3ce3e131989c3e0f339a865d46d
professormood <sdas10560@gmail.com>
2023-12-10 01:25:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      GrayishBlue: "hsl(217, 19%, 38%)",
+      DarkGrayishBlue: "hsl(217, 19%, 24%)",
+      DarkBlue: "hsl(218, 23%, 16%)",
+      LightCyan: "hsl(193, 38%, 86%)",
+      NeonGreen: "hsl(150, 100%, 66%)",
+    },
+    fontFamily:{
+      Manrope:"Manrope"
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
